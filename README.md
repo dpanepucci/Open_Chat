107aa157b4a1b00c03e91c3740295e9cfde10d0d
Dylan Panepucci <dylan.panepucci@gmail.com>
2026-03-24 18:04:04 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Open chat is an application that allows users to message one another.
+
+
+installed socket.io on both server and client side.
---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Open Chat</h1>
+    <aside>
+      <form>
+        <textarea></textarea>
+        <br></br>
+        <button>Send</button>
+      </form>
+    </aside>
+    </>
+  )
+}
+
+export default App
