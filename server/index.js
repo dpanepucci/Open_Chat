78107dc95f19b5662f1c97994d18c82c9035739c
Dylan Panepucci <dylan.panepucci@gmail.com>
2026-03-28 14:56:01 -0400

    installed socket.io for communication rooms. Allowing users to talk to one another within the app.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 import express from 'express';
-const app = express();
-const port = 3000
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
-app.get('/', (req,res) => {
-    res.send('Hello World!')
+const app = express();
+const server = createServer(app);
+const port = 3000;
+const io = new Server(server);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Listening on port http://localhost:${port}`)
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}`);
 });
